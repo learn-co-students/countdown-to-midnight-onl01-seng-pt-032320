909ce151b7339b33bbbb66c55af16db39542a614
Curtis Chadwell <laurencechadwell@gmail.com>
2020-03-24 04:01:46 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
+def countdown_with_sleep(seconds)
+  sleep(seconds)
+end
 
-def countdown
+
+def countdown(integer)
+   while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+    countdown_with_sleep(1)
+   end
+   return "HAPPY NEW YEAR!"
 end
