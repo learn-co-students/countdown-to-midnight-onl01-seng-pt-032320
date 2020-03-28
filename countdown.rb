423d488da629ecbef0a88d07f6fb0871a58fc616
Allan Farca <allan.farca@gmail.com>
2020-03-28 22:01:50 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
-
-def countdown
+number = 10
+def countdown (number)
+  
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  
+    def countdown_with_sleep (number)
+    
+      5.times do 
+        sleep (1)
+      end
+    
+    end
+  
+  end
+  return "HAPPY NEW YEAR!"
 end
+
