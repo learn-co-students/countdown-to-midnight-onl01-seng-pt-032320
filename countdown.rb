09b283085a38d1d0b243d2dce7f32e5dd7838eb9
aurel fontaine <ajfame11@gmail.com>
2020-03-29 19:17:53 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
-
-def countdown
+def countdown(n)
+  n = 10
+  while n > 0
+  puts "#{n} SECOND(S)!"
+  n -= 1
+  end
+return "HAPPY NEW YEAR!"
 end
+def countdown_with_sleep(sleep)
+    sleep(5)
+end
+
+
+  
